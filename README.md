05b6702c8ba48121eb82715d12e8600a5c406d93
Zhandos Usen <usenzhandos@hotmail.com>
2022-11-03 17:03:53 +0600

    Initial

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-Demo repository for [Best Practices When Containerizing Your Next.js Application](https://docker.events.cube365.net/dockercon/2022/content/Videos/42d223c2-e282-4ccb-a364-590aab17efb7) talk at [DockerCon 2022](https://docker.events.cube365.net/dockercon/2022).
+##NGINX reverse proxy and Nextjs app
 
-## To Start
+#To Start
 
 Clone the repo, and run `docker-compose up -d` from the root directory to start the services.
+
+##To Check
+Go to http://localhost:80 url
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,49 +1,9 @@
-import type { NextPage } from 'next'
 import Link from 'next/link'
-import { Layout } from '@components/Layout'
-import { useCookie } from '@hooks/cookie'
 
-const Home: NextPage = () => {
-    const cookieHook = useCookie()
-
-    return (
-        <Layout>
-            <div className="space-between mb-20 flex w-2/4 items-center justify-end px-48 text-center">
-                {!cookieHook.hasUser ? (
-                    <button onClick={() => cookieHook.callRoute('login')} className="mr-3 font-bold text-blue-600">
-                        Login
-                    </button>
-                ) : (
-                    <button onClick={() => cookieHook.callRoute('logout')} className="mr-3 font-bold text-blue-600">
-                        Logout
-                    </button>
-                )}
-            </div>
-            <h1 className="text-6xl font-bold">Next.js Demo</h1>
-
-            <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-                <div className="mt-6 w-96 rounded-xl border p-6 text-left">
-                    <Link href="/blog.html" passHref={true}>
-                        <a className="text-2xl font-bold text-blue-600 hover:text-black focus:text-black">
-                            Explore the Blog
-                        </a>
-                    </Link>
-                    <p className="mt-4 text-xl">Find resources on technical subjects.</p>
-                </div>
-
-                {cookieHook.hasUser && (
-                    <div className="mt-6 w-96 rounded-xl border p-6 text-left">
-                        <Link href="/user-stories">
-                            <a className="text-2xl font-bold text-blue-600 hover:text-black focus:text-black">
-                                User Stories
-                            </a>
-                        </Link>
-                        <p className="mt-4 text-xl">Hear more from happy clients.</p>
-                    </div>
-                )}
-            </div>
-        </Layout>
-    )
+export default function IndexPage() {
+  return (
+    <div>
+      Hello World. <Link href="/about">About</Link>
+    </div>
+  )
 }
-
-export default Home
