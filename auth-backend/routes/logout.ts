6e1366daa4bf1cb4aa526983544938d2b6c3ca44
Zhandos Usen <usenzhandos@hotmail.com>
2022-11-03 17:04:57 +0600

    Initial

--- auth-backend/routes/logout.ts
+++
@@ -1,8 +0,0 @@
-import { Router, Handler } from 'express'
-
-export default (router: Router): Handler | void =>
-    router.get('/logout', (request, response, next) => {
-        response.clearCookie('loginCookie')
-        response.status(200).send()
-        next()
-    })
